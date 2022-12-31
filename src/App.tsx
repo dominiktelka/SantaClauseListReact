4ab9a5a8486308a4321b1420f6a0c0a2aad90aa2
Dominik Telka <dominiktelka@gmail.com>
2022-12-31 12:57:41 +0100

    rename files Test to ChildView and added SingleGiftView

--- src/App.tsx
+++ src/App.tsx
@@ -5,16 +5,18 @@ import {GiftsView} from './views/GiftsView'
 
 
 import {Route, Routes} from 'react-router-dom';
-import {TestView} from "./views/TestView";
+import {ChildView} from "./views/ChildView";
 import {Header} from "./components/Header/Header";
 import { NotFoundView } from './views/NotFoundView';
+import { SingleGiftView } from './views/SingleGiftView';
 
 export const App = () => {
   return (<>
           <Header/>
           <Routes>
               <Route path='/gift' element={<GiftsView/>}></Route>
-              <Route path='/test' element={<TestView/>}></Route>
+              <Route path='/gift/:idOfGift' element={<SingleGiftView/>}></Route>
+              <Route path='/child' element={<ChildView/>}></Route>
               <Route path='*' element={<NotFoundView/>}></Route>
           </Routes>
       </>
