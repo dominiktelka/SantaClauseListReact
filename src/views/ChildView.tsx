de3855d48b50d13213b925008859ea2c1bc3f8f5
Dominik Telka <dominiktelka@gmail.com>
2022-12-31 13:03:58 +0100

    delete unused Link and renamed to ChildView

--- src/views/ChildView.tsx
+++ src/views/ChildView.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
-export const TestView = () =>{
+
+export const ChildView = () =>{
+
     return (
         <>
             <h2>Test!</h2>
+
         </>
     )
 
