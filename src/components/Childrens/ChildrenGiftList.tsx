b7a13f99670f7efab2469b98d4920446adae8e79
Dominik Telka <dominiktelka@gmail.com>
2023-01-08 13:33:31 +0100

    created whole file which is responsible for showing list with gifts and give us chance to change gift for child. It refresh every time when u change the gift for child

--- src/components/Childrens/ChildrenGiftList.tsx
+++ src/components/Childrens/ChildrenGiftList.tsx
@@ -1,1 +1,49 @@
-import React from 'react';
+import React, {FormEvent, useState} from 'react';
+import { GiftEntity, SetGiftForChildReq} from 'types';
+
+interface Props{
+    giftsList: GiftEntity[];
+    selectedId: string;
+    childId: string;
+    onGiftChange: () => void;
+}
+export const ChildrenGiftList = (props: Props) =>{
+    const[selected,setSelected] = useState<string>(props.selectedId)
+
+    const sendForm = async(e:FormEvent) =>{
+        e.preventDefault();
+
+
+    const res = await fetch(`http://localhost:3001/children/gift/${props.childId}`, {
+        method: 'PATCH',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            giftId: selected,
+        } as SetGiftForChildReq)
+    });
+
+    if([400,500].includes(res.status)){
+        const error = await res.json()
+        alert(`Error occured: ${error.message}`);
+        return
+    }
+        props.onGiftChange();
+    }
+
+    return (<form onSubmit={sendForm}>
+        <select value={selected} onChange={e=> setSelected(e.target.value)}>
+            {
+                props.giftsList
+                    .map(gift => <option
+                        value={gift.id}
+                        key={gift.id}
+                    >{gift.name}
+                    </option>)
+            }
+        </select>
+        <button type='submit'>Change gift</button>
+    </form>)
+}
+
