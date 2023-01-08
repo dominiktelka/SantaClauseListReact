6f4a9037ad9b0ccaa8a332df4c080349f89b29eb
Dominik Telka <dominiktelka@gmail.com>
2023-01-08 13:36:07 +0100

    change way how ChildrenList is taken from backend, also added props onGiftChange to others components

--- src/components/Childrens/ChildrenList.tsx
+++ src/components/Childrens/ChildrenList.tsx
@@ -1,34 +1,33 @@
 import React, {useEffect, useState} from 'react';
-import {ChildEntity, GiftEntity} from 'types'
+import {ListChildrenRes} from 'types'
 
 
 import {Spinner} from "../common/Spinner/Spinner";
 import { ChildrenTable } from './ChildrenTable';
 
 export const ChildrensList = () =>{
-    const [childrenList, setChildrensList] = useState<ChildEntity[] | null>(null);
-    const [gift, setGift] = useState<GiftEntity[] | null>(null);
+
+    const [data, setData] = useState<ListChildrenRes | null>(null)
 
 
-    const refreshChildrens = async () =>{
-        setChildrensList(null)
-        setGift(null)
+    const refreshChildren = async () =>{
+        setData(null)
         const res = await fetch('http://localhost:3001/children');
-        const data = await res.json();
-        setChildrensList(data.childrenList)
-        setGift(data.giftsList)
+        setData(await res.json())
     };
 
     useEffect(()=>{
-        refreshChildrens();
+        refreshChildren();
     },[])
-    if(childrenList === null){
+    if(data === null){
         return <Spinner/>
     }
 
     return <>
         <h1>Childrens</h1>
-        <ChildrenTable children={childrenList}
-                       onChildrenChange={refreshChildrens}/>
+        <ChildrenTable childrenList={data.childrenList}
+                       giftsList={data.giftList}
+                       onGiftChange={refreshChildren}
+        />
     </>
 }
